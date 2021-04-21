275470e666123cad7a75e12bcde7b2ba2a0125e4
Mikita Slabysh <slabysh2015@gmail.com>
2021-04-21 03:53:58 -0400

    Clear Console, Rerun App
    Delete old messages, Rerun app after passwords have been generated, identifiers next to each password type

--- passwordgenerator.py
+++ passwordgenerator.py
@@ -5,6 +5,7 @@
 #     Last Update: April 10th 2021
 #$&#$&#$&#$&#$&#$&#$&#$&#$&#$&#$&#$&#$&#$&#$&#$&#$&#$&#$&#
 import random
+import os
 #Variables
 uppercase = 0
 lowercase = 0
@@ -14,8 +15,10 @@ length = 0
 special = []#Array for special chars
 numchars = 0
 pass_num = 0
+settings = 0
 rand_chars = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890#$&*@!"
 num_chars = "1234567890"
+clear = lambda: os.system("cls")#Clear out console
 #Check if input is a number
 def check_int(var, prompt):
   while True:
@@ -43,115 +46,128 @@ def random_password_gen(uppercase, lowercase, numbers, numchars):
     password += special[b]
   final = list(password)
   random.shuffle(final)#Shuffle characters in password
+  print("Here is your Custom Random Passwords: ")
   print("".join(final))#Convert List back to string
 #Number and Random Password Generator
-def simple_pas_gen(chars, number_of_passwords, length):
-  print("Here are your passwords: ")
+def simple_pas_gen(chars, number_of_passwords, length, name: str):
+  print("Here are your {0} passwords: ".format(name))
   #Generate Passwords
   for x in range(number_of_passwords):
     password = ""#Store password and reset it
     for y in range(length):
       password += random.choice(chars)
     print(password)
-#Settings
-print("""
-Custom Password Generator
-What do you want your password to be ?
-  1. Custom Random Password
-  2. Password with Familiar Word(Under Development)
-  3. Number Password
-  4. Random Password
-""")
-settings = int(input())
-#Length for Random and Number passwords
-if settings == 1 or settings == 3 or settings == 4:
-  length = check_int(length,"How long would you like your password to be ?")
-  lowercase = length
-  available = length#Amount of characters we can change
-  #Number of passwords at a Time
-  pass_num = int(input("How many passwords would you like to be generated ?"))
-  while pass_num == 0:
-    if pass_num == 0:
-      pass_num = int(input("Input cannot be 0, please try again"))
+#Application
+def run_app(): 
+  global uppercase, lowercase, numbers, numchars, settings, available, special, pass_num, length
+  #Settings
+  print("""
+  Custom Password Generator
+  By DeadAnarch1st
+  What do you want your password to be ?
+    1. Custom Random Password
+    2. Password with Familiar Word(Under Development)
+    3. Number Password
+    4. Random Password
+  """)
+  settings = check_int(settings, "Enter your choice: ")
+  #Length for Random and Number passwords
+  if settings == 1 or settings == 3 or settings == 4:
+    length = check_int(length,"How long would you like your password to be ?")
+    lowercase = length
+    available = length#Amount of characters we can change
+    #Number of passwords at a Time
+    pass_num = check_int(pass_num, "How many passwords would you like to be generated ?")
+    while pass_num == 0:
+      if pass_num == 0:
+        pass_num = check_int(pass_num, "Input cannot be 0, please try again")
+    clear()
 
-#Settings for Random Password
-if settings == 1:
-  print("Length: " + str(length))
-  print("Lowercase Letters: " + str(lowercase))
-  print("Available Character Spaces: " + str(available))
-  uppercase = check_int(uppercase,"How many characters would you like to be uppercase ?")
-  #Check if user puts more uppercase chars than available
-  if uppercase > available:
-    while uppercase > available:
-      print("Too many uppercase characters, enter a number less than " + str(available))
-      uppercase = check_int(uppercase,"How many characters would you like to be uppercase ?")
-  lowercase = lowercase - uppercase
-  available = available - uppercase
-
-  #Check if there are any available spaces left
-  if available > 0:
+  #Settings for Random Password
+  if settings == 1:
     print("Length: " + str(length))
     print("Lowercase Letters: " + str(lowercase))
-    print("Uppercase Letters: " + str(uppercase))
-    print("Available Spaces: " + str(available))
-    numbers = check_int(numbers,"How many characters would you like to be numbers ?")
-    if numbers > available:
-      while numbers > available:
-        print("Too many number characters, enter a number less than " + str(available))
-        numbers = check_int(numbers,"How many characters would you like to be numbers ?")
-    lowercase = lowercase - numbers
-    available = available - numbers
+    print("Available Character Spaces: " + str(available))
+    uppercase = check_int(uppercase,"How many characters would you like to be uppercase ?")
+    #Check if user puts more uppercase chars than available
+    if uppercase > available:
+      while uppercase > available:
+        print("Too many uppercase characters, enter a number less than " + str(available))
+        uppercase = check_int(uppercase,"How many characters would you like to be uppercase ?")
+    lowercase = lowercase - uppercase
+    available = available - uppercase
+    clear()
 
-  if available > 0:
-    print("Length: " + str(length))
-    print("Lowercase Letters: " + str(lowercase))
-    print("Uppercase Letters: " + str(uppercase))
-    print("Number Chars: " + str(numbers))
-    print("Available Spaces: " + str(available))
-    chars = input("Would you like special characters in your password ? yes/no")
-    chars = chars.lower()#Convert input into lowercase
-    if chars == "yes":#User wants special chars
-      numchars = check_int(numchars,"How many special characters would you like ?")
-      if numchars > available:#Check if user wants to enter more characters than available
-        while numchars > available:#If user is stupid and keeps trying to enter too many chars
-          print("Too many special characters, please enter different number")
-          numchars = check_int(numchars,"How many special characters would you like ?")
-      for x in range(numchars):#Loop for however many special chars user wants
-        counter = x + 1
-        signs = input("Input your #" + str(counter) + " special character: ")
-        special.append(signs)#Store special chars in array
-    else:
-      pass#Skip
-  #Show Random Password
-  print("Here is your passwords: ")
-  for i in range(pass_num):
-    random_password_gen(uppercase, lowercase, numbers, numchars)
-#Generate Number Password
-if settings == 3:
-  simple_pas_gen(num_chars, pass_num, length)
-#Generate Random Password
-if settings == 4:
-  simple_pas_gen(rand_chars, pass_num, length)
-#Password with Familiar Word
-if settings == 2:
-  word = input("What word do you want to use for your password ?")
-  #Check if the word is valid
-  w_len = len(word)#Get length of the word
-  if w_len < 3 or w_len > 15:
-    while w_len < 3 or w_len > 15:
-      word = input("Your word is invalid, please enter a word 3 to 15 characters long")
-      w_len = len(word)
-  
-  #Settings for the word
-  print("Length of the password: " + str(w_len)) 
-  print("Password so far: " + word)
-  uppercase = int(input("How many characters you want to be uppercase ?"))
-  if uppercase > w_len:
-    while uppercase > w_len:
-      uppercase = int(input("Invalid number, enter number less than length of the word"))
-  #APPLY UPPERCASE SETTINGS TO THE WORD-----------------------
-  print("Password so far: " + word)
-  
+    #Check if there are any available spaces left
+    if available > 0:
+      print("Length: " + str(length))
+      print("Lowercase Letters: " + str(lowercase))
+      print("Uppercase Letters: " + str(uppercase))
+      print("Available Spaces: " + str(available))
+      numbers = check_int(numbers,"How many characters would you like to be numbers ?")
+      if numbers > available:
+        while numbers > available:
+          print("Too many number characters, enter a number less than " + str(available))
+          numbers = check_int(numbers,"How many characters would you like to be numbers ?")
+      lowercase = lowercase - numbers
+      available = available - numbers
+      clear()
+
+    if available > 0:
+      print("Length: " + str(length))
+      print("Lowercase Letters: " + str(lowercase))
+      print("Uppercase Letters: " + str(uppercase))
+      print("Number Chars: " + str(numbers))
+      print("Available Spaces: " + str(available))
+      chars = input("Would you like special characters in your password ? yes/no")
+      chars = chars.lower()#Convert input into lowercase
+      if chars == "yes":#User wants special chars
+        numchars = check_int(numchars,"How many special characters would you like ?")
+        if numchars > available:#Check if user wants to enter more characters than available
+          while numchars > available:#If user is stupid and keeps trying to enter too many chars
+            print("Too many special characters, please enter different number")
+            numchars = check_int(numchars,"How many special characters would you like ?")
+        for x in range(numchars):#Loop for however many special chars user wants
+          counter = x + 1
+          signs = input("Input your #" + str(counter) + " special character: ")
+          special.append(signs)#Store special chars in array
+      else:
+        pass#Skip
+    clear()
+    #Show Random Password
+    for i in range(pass_num):
+      random_password_gen(uppercase, lowercase, numbers, numchars)
+    run_app()
+  #Generate Number Password
+  if settings == 3:
+    simple_pas_gen(num_chars, pass_num, length, "Number")
+    run_app()
+  #Generate Random Password
+  if settings == 4:
+    simple_pas_gen(rand_chars, pass_num, length, "Random")
+    run_app()
+  #Password with Familiar Word
+  if settings == 2:
+    word = input("What word do you want to use for your password ?")
+    #Check if the word is valid
+    w_len = len(word)#Get length of the word
+    if w_len < 3 or w_len > 15:
+      while w_len < 3 or w_len > 15:
+        word = input("Your word is invalid, please enter a word 3 to 15 characters long")
+        w_len = len(word)
+    
+    #Settings for the word
+    print("Length of the password: " + str(w_len)) 
+    print("Password so far: " + word)
+    uppercase = int(input("How many characters you want to be uppercase ?"))
+    if uppercase > w_len:
+      while uppercase > w_len:
+        uppercase = int(input("Invalid number, enter number less than length of the word"))
+    #APPLY UPPERCASE SETTINGS TO THE WORD-----------------------
+    print("Password so far: " + word)
+
+run_app()
+    
 
 
 
